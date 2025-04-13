462b379cf8c5306639d3bef6a3bd896960edada1
Kariaki58 <kariakistephen809@gmail.com>
2025-04-13 20:52:04 +0100

    added gitignore file

---
+++ index.js
@@ -0,0 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: "YOUR_API_KEY" });
+console.log(process.env.GEMINI_API_KEY);
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: "Explain how AI works",
+    });
+    console.log(response.text);
+}
+
+await main();
